9c18afa608cb391ef52467b3b30be68a3b5c06f4
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-09-27 22:43:35 +0545

    created animals table as per requirement

---
+++ schema.sql
@@ -0,0 +1,14 @@
+/* Database schema to keep the structure of entire database. */
+
+CREATE DATABASE vet_clinic;
+\connect vet_clinic
+
+CREATE TABLE animals (
+    id INT not null,
+    name varchar(100) not null,
+    date_of_births DATE,
+    escape_attempts INT,
+    neutered boolean,
+    weight_kg decimal,
+    PRIMARY KEY (id)
+);
