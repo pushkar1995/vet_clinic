9454eea92b2f8c7ed907368dbe2df7468388f40e
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-10-04 14:50:05 +0545

    Minor fix on files and created schema_based_on_diagram.sql file

--- queries.sql
+++ queries.sql
@@ -205,3 +205,7 @@ WHERE vets.name = 'Maisy Smith'
 GROUP BY species.name
 ORDER BY visit_count DESC
 LIMIT 1;
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits WHERE animal_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits WHERE vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners WHERE email = 'owner_18327@mail.com';
